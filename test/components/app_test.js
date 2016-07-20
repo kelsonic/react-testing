2990118b169b7bb251ecd19b859d769c2157a920
kelsonic <kgadams93@gmail.com>
2016-07-20 14:48:57 +0200

    Add test to check comment box component on app component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,17 +1,17 @@
 import {renderComponent, expect} from '../test_helper';
 import App from '../../src/components/app';
 
-// Use describe to group together similar tests
+
 describe('App', () => {
 
-  // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-    
-    const component = renderComponent(App);
+  let component;
 
-    // Use 'expect' to make an assertion about a target
-    expect(component).to.contain('React simple starter');
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 
 });
